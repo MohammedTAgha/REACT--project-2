ecdc4f09450923bcdccf0996e4151b1119592d45
MOHAMMED T AGHA <mohammedtabsemagha@gmail.com>
2023-09-14 13:02:05 +0300

    set up next and base things
    redux store + use auth + useApi + mock +

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,24 @@
+"use client";
+import { Input } from "../components/atoms/Input";
+
+import { redirect } from "next/navigation";
+import useAuth from "../hooks/useAuth";
+import { useEffect } from "react";
 
 export default function Home() {
+  const {token} = useAuth()
+  // it's funny to do this 🙄
+  // next middleware resolve this problem , i'll learn soon
+  useEffect(()=>{
+    if(token){
+      redirect('/home')
+    }
+    else {
+      redirect('/login')
+    }
+  },[token])
   return (
     <main>
-    
     </main>
-  )
+  );
 }
